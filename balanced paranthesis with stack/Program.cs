52fdde4ad348b4279d29196d0110ab31b8e6df26
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2023-08-11 11:39:56 +0430

    Updated

---
+++ balanced paranthesis with stack/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string of paranthesis : ");
+        var inputString = Console.ReadLine();
+
+        var result = checkBalance(inputString);
+        Console.WriteLine(result);
+    }
+    static bool checkBalance(string inputString)
+    {
+        var isBalanced = false;
+        var myStack = new Stack();
+
+        if (inputString.Length == 0 || inputString.Length % 2 != 0 || inputString[0] == ')')
+        {
+        }
+        else
+        {
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == '(')
+                {
+                    myStack.Push(inputString[i]);
+                }
+                else if (inputString[i] == ')')
+                {
+                    myStack.Pop();
+                }
+            }
+            if (myStack.Count == 0)
+            {
+                isBalanced = true;
+            }
+        }
+        return isBalanced;
+    }
+}
---
+++ balanced parenthesis/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var isBalanced = false;
+        int m = 0; // For '('
+        int n = 0; // For ')'
+
+        Console.WriteLine("Enter a string : ");
+        var input = Console.ReadLine();
+
+        if (input[0] != '(' || input.Length % 2 != 0)
+        {
+        }
+        else
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    m++;
+                }
+                else if (input[i] == ')')
+                {
+                    n++;
+                }
+            }
+            if (m == n)
+            {
+                isBalanced = true;
+            }
+        }
+        Console.WriteLine(isBalanced);
+    }
+}
